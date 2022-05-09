7c69f22b0a784dc7c0c028dc4ea3236866b56d12
juancrls <juan1112@outlook.com.br>
2022-05-09 17:07:15 -0300

    added main information styles

--- cart.js
+++ cart.js
@@ -1,63 +1,63 @@
-let cartQuantity = 0;
-let cart = document.querySelector(".shopping-cart");
-let lightBox = document.querySelector(".lightbox-container");
+// let cartQuantity = 0;
+// let cart = document.querySelector(".shopping-cart");
+// let lightBox = document.querySelector(".lightbox-container");
 
-cart.addEventListener("click", () => {
-    lightBox.classList.toggle("active");
-})
+// cart.addEventListener("click", () => {
+//     lightBox.classList.toggle("active");
+// })
 
-let cartContent = document.querySelector(".cart-content");
-let emptyCartMessage = document.querySelector(".cart-empty");
+// let cartContent = document.querySelector(".cart-content");
+// let emptyCartMessage = document.querySelector(".cart-empty");
 
-if(cartQuantity == 0) {
-    cartContent.style.display = "none";
-    emptyCartMessage.style.display = "flex";
-} else {
-    cartContent.style.display = "block";
-    emptyCartMessage.style.display = "none";
-}
+// if(cartQuantity == 0) {
+//     cartContent.style.display = "none";
+//     emptyCartMessage.style.display = "flex";
+// } else {
+//     cartContent.style.display = "block";
+//     emptyCartMessage.style.display = "none";
+// }
 
 
 
-let addItem = document.querySelector(".add-item");
-let removeItem = document.querySelector(".remove-item");
-let removeAll = document.querySelector(".delete-item-icon");
+// let addItem = document.querySelector(".add-item");
+// let removeItem = document.querySelector(".remove-item");
+// let removeAll = document.querySelector(".delete-item-icon");
 
-let cartQuantityNotification = document.querySelector(".shopping-cart .item-quantity");
+// let cartQuantityNotification = document.querySelector(".shopping-cart .item-quantity");
 
-let itemQuantity = document.querySelectorAll(".item-quantity")
+// let itemQuantity = document.querySelectorAll(".item-quantity")
 
-addItem.addEventListener("click", () => {
-    if(cartQuantity >= 10) return;
+// addItem.addEventListener("click", () => {
+//     if(cartQuantity >= 10) return;
 
-    cartQuantity++;
-    itemQuantity.forEach(quantity => quantity.innerHTML = cartQuantity);
-    cartQuantityNotification.style.display = "flex";
+//     cartQuantity++;
+//     itemQuantity.forEach(quantity => quantity.innerHTML = cartQuantity);
+//     cartQuantityNotification.style.display = "flex";
 
-    cartContent.style.display = "block";
-    emptyCartMessage.style.display = "none";
-})
+//     cartContent.style.display = "block";
+//     emptyCartMessage.style.display = "none";
+// })
 
-removeItem.addEventListener("click", () => {
-    cartQuantity--;
-    itemQuantity.forEach(quantity => quantity.innerHTML = cartQuantity);
+// removeItem.addEventListener("click", () => {
+//     cartQuantity--;
+//     itemQuantity.forEach(quantity => quantity.innerHTML = cartQuantity);
 
-    if(cartQuantity == 0) {
-        cartContent.style.display = "none";
-        cartQuantityNotification.style.display = "none";
-        emptyCartMessage.style.display = "flex";
+//     if(cartQuantity == 0) {
+//         cartContent.style.display = "none";
+//         cartQuantityNotification.style.display = "none";
+//         emptyCartMessage.style.display = "flex";
 
-        return;
-    }
-})
+//         return;
+//     }
+// })
 
-removeAll.addEventListener("click", () => {
-    cartQuantity = 0;
-    itemQuantity.forEach(quantity => quantity.innerHTML = 0);
+// removeAll.addEventListener("click", () => {
+//     cartQuantity = 0;
+//     itemQuantity.forEach(quantity => quantity.innerHTML = 0);
 
-    cartContent.style.display = "none";
-    cartQuantityNotification.style.display = "none";
-    emptyCartMessage.style.display = "flex";
+//     cartContent.style.display = "none";
+//     cartQuantityNotification.style.display = "none";
+//     emptyCartMessage.style.display = "flex";
 
-    return;
-})
+//     return;
+// })
